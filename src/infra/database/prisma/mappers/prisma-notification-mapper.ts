a38d46048caf1fa9fbba67a8397b392cda9c0fbc
Paulo Nascimento <paulovictor.alu.lmb@gmail.com>
2023-01-04 13:09:22 -0300

    feat: update mappers

--- src/infra/database/prisma/mappers/prisma-notification-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-notification-mapper.ts
@@ -1,4 +1,6 @@
-import { Notification } from '@application/entities/notification';
+import { Notification as RawNotification } from '@prisma/client';
+import { Content } from 'src/application/entities/content';
+import { Notification } from '../../../../../src/application/entities/notification';
 
 export class PrismaNotificationMapper {
   static toPrisma(notification: Notification) {
@@ -10,5 +12,18 @@ export class PrismaNotificationMapper {
       readAt: notification.readAt,
       createdAt: notification.createdAt,
     };
+  }
+
+  static toDomain(raw: RawNotification): Notification {
+    return new Notification(
+      {
+        category: raw.category,
+        content: new Content(raw.content),
+        recipientId: raw.recipientId,
+        readAt: raw.readAt,
+        createdAt: raw.createdAt,
+      },
+      raw.id,
+    );
   }
 }
